b77146b80f3cccad1e668da074e2a86bc13f3871
Schwyn Francis <schwynf@gmail.com>
2020-06-13 14:36:19 -0700

    added spinner for pwned button

--- controller/pwned-controller.js
+++ controller/pwned-controller.js
@@ -1,51 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 const CryptoJs = require("crypto-js");
-
 const pwnedKey = process.env.API_KEY;
-
 router.get("/api/pwned/password/:id", async (req, res) => {
   try {
     const hashed = CryptoJs.SHA1(req.params.id).toString();
-
-    const firstFive = hashed.slice(0, 5)
-
+    const firstFive = hashed.slice(0, 5);
     const rest = hashed.slice(5, hashed.length).toUpperCase();
-
     const data = await axios.get(`https://api.pwnedpasswords.com/range/${firstFive}`, {
       headers: {
         "Content-Type": "application/json",
         "hibp-api-key": pwnedKey
       }
     });
-    const resArr = data.data.split("\n")
+    const resArr = data.data.split("\n");
     for (let index = 0; index < resArr.length; index++) {
-      const sliced = resArr[index].slice(0,35)
+      const sliced = resArr[index].slice(0,35);
      if (sliced == rest) {
-       console.log("_____")
-       console.log("matched")
-       res.json(sliced)
+       return res.json(sliced);
      }
     }
+    return res.json({match: "NO-MATCH"});
   } catch (error) {
     console.error(error);
   }
 });
-
 router.get("/api/pwned/email/:id", async (req, res) => {
   try {
-    const breaches = [];
+    // const breaches = [];
     const data = await axios.get(`https://haveibeenpwned.com/api/v3/breachedaccount/${req.params.id}`, {
       headers: {
         "Content-Type": "application/json",
         "hibp-api-key": pwnedKey
       }
     });
-
     res.json(data.data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 module.exports = router;
--- public/js/pwned.js
+++ public/js/pwned.js
@@ -2,11 +2,11 @@ const pwnedAPI = {
   submitPass: async () => {
     const passText = pwnedPassword.val().trim();
     if (passText === "Please enter a valid password" || passText === "") {
-      pwnedPassword.val("Please enter a valid password")
-      pwnedPassword.addClass("invalid")
+      pwnedPassword.val("Please enter a valid password");
+      pwnedPassword.addClass("invalid");
     }
     else {
-      console.log("no")
+      // console.log("no");
       pwnedAPI.getPwnedPass(passText);
       pwnedPassword.val("");
     }
@@ -15,33 +15,43 @@ const pwnedAPI = {
     const emailText = pwnedEmail.val().trim();
     if (validateEmail(emailText)) {
       pwnedAPI.getPwnedEmail(emailText);
-      pwnedEmail.val("")
+      pwnedEmail.val("");
     }
     else {
       pwnedEmail.val("Please enter a valid email");
-      pwnedEmail.addClass("invalid")
-      return
+      pwnedEmail.addClass("invalid");
+      return;
     }
   },
   getPwnedEmail: async (email) => {
     const data = await $.get("/api/pwned/email/" + email);
-    console.log(data);
+    // console.log(data);
   },
   getPwnedPass: async (password) => {
+    //spinner installed
+    let eleI = $("<i>");
+    eleI.addClass("fa fa-spinner fa-spin");
+    eleI.css("font-size", "24px");
+    $("#pwned-pass-btn").append(eleI);
+
     const data = await $.get("/api/pwned/password/" + password);
-    pwnedAPI.displayPassword(data)
+    //spinner uninstalled
+    $(".fa-spinner").remove();
+
+    if (data.match != "NO-MATCH"){
+      pwnedAPI.displayPassword(data);
+    }
   },
   displayPassword: (data) => {
-    pwnedDiv.empty()
+    pwnedDiv.empty();
     if (data.length > 0) {
       const pEl = $("<p>")
       .text(`Oh no! Its looks like we found ${data.length} matches. Consider changing your credentials`)
       .attr({
         class: "text-center pt-3"
-      })
-      pwnedDiv.append(pEl)
-      pwnedCard.addClass("danger")
+      });
+      pwnedDiv.append(pEl);
+      pwnedCard.addClass("danger");
     }
   }
-
 };
